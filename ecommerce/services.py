c060f542635f6e0e8685f1668aa61c8d7fb1a3c7
Miller Quilindo <mdquilindo@unicauca.edu.co>
2023-08-08 03:13:14 -0500

    catch exception token

--- ecommerce/services.py
+++ ecommerce/services.py
@@ -8,13 +8,14 @@ import logging
 
 def get_token_epayco():
     session = requests.Session()
-    session.headers.update({'Authorization': 'Basic ' + base64.b64encode((settings.PUBLIC_KEY + ":" + settings.PRIVATE_KEY).encode('utf-8')).decode('utf-8')})
+    session.headers.update({'Authorization': 'Basic ' + base64.b64encode((settings.PUBLIC_KEY + ":" + settings.PRIVATE_KEY).encode('utf-8')).decode('utf-8'), 'Content-Type': 'application/json'})
     session.headers.update({'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'})
     try:
         r = session.post('https://apify.epayco.co/login', data='')
         if r.status_code == 200:
             return r.json()['token']
     except requests.exceptions.RequestException as e:
+        logging.error('Error get token', e)
         return None
 
 
