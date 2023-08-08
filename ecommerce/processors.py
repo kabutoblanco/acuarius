044130130405e677eaf5d4b9133884acdfb9457b
Miller Quilindo <mdquilindo@unicauca.edu.co>
2023-08-08 01:30:57 -0500

    load session for guest

--- ecommerce/processors.py
+++ ecommerce/processors.py
@@ -2,6 +2,8 @@ from .models import Category, CartProduct
 
 
 def navbar(request):
+    if not request.session.session_key:
+        request.session['guest'] = True
     device = request.COOKIES.get('sessionid', '')
     categories = Category.objects.all()
     cart_count = CartProduct.objects.filter(customer__uid_device=device).count()
