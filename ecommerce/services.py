a6f005a25bfdfca22480a18044b89d902efc2d5e
Miller Quilindo <mdquilindo@unicauca.edu.co>
2023-08-08 22:57:20 -0500

    primer paso ceo

--- ecommerce/services.py
+++ ecommerce/services.py
@@ -3,29 +3,20 @@ from django.conf import settings
 import requests
 import base64
 import json
-import logging
 
 
 def get_token_epayco():
     public_key = settings.PUBLIC_KEY
     private_key = settings.PRIVATE_KEY
-    logging.warning("cadenas: ")
-    logging.warning(public_key)
-    logging.warning(private_key)
     credentials = f"{public_key}:{private_key}"
-    logging.warning(credentials)
     encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
 
     session = requests.Session()
     session.headers.update({'Authorization': 'Basic ' + encoded_credentials, 'Content-Type': 'application/json'})
-    session.headers.update({'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'})
-    try:
-        r = session.post('https://apify.epayco.co/login', data='')
-        if r.status_code == 200:
-            return r.json()['token']
-    except requests.exceptions.RequestException as e:
-        logging.error('Error get token', e)
-        return None
+    r = session.post('https://apify.epayco.co/login', data='')
+    if r.status_code == 200:
+        return r.json()['token']
+    return None
 
 
 def payment_pse(token, payload):
