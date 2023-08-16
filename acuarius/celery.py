a8acefc0f6ab5555c74f707642d1bd577bb042bd
Miller Quilindo <mdquilindo@unicauca.edu.co>
2023-08-15 20:54:16 -0500

    celery conf

---
+++ acuarius/celery.py
@@ -0,0 +1,32 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acuarius.settings')
+
+app = Celery('acuarius')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
+
+
+app.conf.beat_schedule = {
+    'check_payments': {
+        'task': 'check_payments',  
+        'schedule': 60.0 * 30.0,
+        'args': () 
+    },
+}  
