0bc477028aedcdd382b323e05ef71aafc5a96436
Miller Quilindo <mdquilindo@unicauca.edu.co>
2023-08-28 15:08:08 -0500

    set redirect

--- acuarius/urls.py
+++ acuarius/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ecommerce.urls'))
+    path('', include('ecommerce.urls')),
+    path('acuariusfloristeriacali.com/', RedirectView.as_view(url='http://www.acuariusfloristeriacali.com/', permanent=True)),
 ]
 
 if settings.DEBUG:
