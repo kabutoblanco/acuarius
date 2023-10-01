224e8d4e15b7058c14854d79de6f6dbdfe012bc8
Miller Quilindo <mdquilindo@unicauca.edu.co>
2023-10-01 09:51:57 -0500

    view url bank

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -285,6 +285,7 @@ class PaymentView(CreateView):
                 payment.save()
                 check_payments.apply_async(countdown=60*5)
                 CartProduct.objects.filter(customer__uid_device=device).delete()
+                print(payment_response['data']['urlbanco'])
                 return redirect(payment_response['data']['urlbanco'])
             else:
                 return redirect('/carrito')
